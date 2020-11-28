33a9113dcd281bbecb1e7b10a0e3dbd1d3c751f3
KrisAstro <75182200+KrisAstro@users.noreply.github.com>
2020-11-28 21:38:45 +0100

    Add files via upload

---
+++ config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define INDI Data Dir */
+#cmakedefine INDI_DATA_DIR "@INDI_DATA_DIR@"
+/* Define Driver version */
+#define ARDUITOR_VERSION_MAJOR @ARDUITOR_VERSION_MAJOR@
+#define ARDUITOR_VERSION_MINOR @ARDUITOR_VERSION_MINOR@
+
+#endif // CONFIG_H
